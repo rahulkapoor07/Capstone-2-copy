c049e1ce63f154d95aa0817e1ab37e2333bd681a
rahul <rahulkapoor07@gmail.com>
2022-02-16 04:28:28 -0800

    some change in db file

--- db.js
+++ db.js
@@ -2,7 +2,10 @@ const {Pool} = require("pg");
 const {DB_URI} = require("./config");
 
 let db = new Pool({
-    connectionString : DB_URI
+    connectionString : DB_URI,
+    ssl: {    /* <----- Add SSL option */
+        rejectUnauthorized: false,
+      }
 });
 
 module.exports = db;
